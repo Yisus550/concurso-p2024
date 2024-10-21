12e7ae1b98e2df3a69f4b699d4f6510752ca095e
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-21 16:23:08 -0700

    Se configuro appsettings.json y Program.cs, se agrego ApplicationDbContext y se instalaron NutGets necesarios

---
+++ concurso-p2024/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace concurso_p2024.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+	}
+}
